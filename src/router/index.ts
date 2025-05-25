06e66db0477116567942626f30dc982ffb53c90f
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-05-25 20:16:46 +0300

    adding single project page component

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const router = createRouter({
       name: 'projects',
       component: () => import('@/views/ProjectsView.vue'),
     },
+    {
+      path: '/projects/:id',
+      name: 'prosingle-project',
+      component: () => import('@/views/SingleProjectView.vue'),
+    },
   ],
 })
 
