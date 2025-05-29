20830351407ae04e62aae946f2b6327bd582fde7
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-05-29 15:34:43 +0300

    Configure Unolug Vue Router to workk with unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,7 +26,7 @@ export default defineConfig({
         /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
         /\.md$/, // .md
       ],
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       dts: './auto-imports.d.ts',
       viteOptimizeDeps: true,
       eslintrc: {
