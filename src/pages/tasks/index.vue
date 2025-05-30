84973b26b2aa481717cf181951c208ae41501d8e
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-05-30 18:19:52 +0300

    code clean up

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -18,28 +18,6 @@ const tasks = ref<Tables<'tasks'>[] | null>(null)
   tasks.value = data
 })()
 
-// interface Payment {
-//   id: string
-//   amount: number
-//   status: 'pending' | 'processing' | 'success' | 'failed'
-//   email: string
-// }
-
-// const payments: Payment[] = [
-//   {
-//     id: '728ed52f',
-//     amount: 100,
-//     status: 'pending',
-//     email: 'm@example.com',
-//   },
-//   {
-//     id: '489e1d42',
-//     amount: 125,
-//     status: 'processing',
-//     email: 'example@gmail.com',
-//   },
-// ]
-
 const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
     accessorKey: 'name',
