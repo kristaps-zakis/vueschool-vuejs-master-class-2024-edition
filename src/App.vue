043c1c821cc6ab2195c15d9b0d13933b42fca825
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-06-04 12:28:13 +0300

    code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <script setup lang="ts">
 import AuthLayout from '@/components/Layout/main/AuthLayout.vue'
-
-// const MockComponent = defineComponent(async () => {
-//   const greeting = ref('Hello, World!')
-
-//   await new Promise((resolve) => {
-//     greeting.value = 'Hello, World! (from async component)'
-
-//     setTimeout(() => {
-//       resolve(true)
-//     }, 1000)
-//   })
-//   return () => h('div', greeting.value)
-// })
 </script>
 
 <template>
   <AuthLayout>
     <Suspense>
-      <!-- <MockComponent /> -->
       <RouterView />
       <template #fallback>
         <div>Loading...</div>
