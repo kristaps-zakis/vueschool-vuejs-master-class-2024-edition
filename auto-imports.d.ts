56c17cba978d74722e1936e89ba781c7987d53e2
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-06-08 13:37:27 +0300

    update customerror interface

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -59,6 +59,7 @@ declare global {
   const useAttrs: typeof import('vue')['useAttrs']
   const useCssModule: typeof import('vue')['useCssModule']
   const useCssVars: typeof import('vue')['useCssVars']
+  const useErrorStore: typeof import('./src/stores/error')['useErrorStore']
   const useId: typeof import('vue')['useId']
   const useLink: typeof import('vue-router')['useLink']
   const useModel: typeof import('vue')['useModel']
@@ -137,6 +138,7 @@ declare module 'vue' {
     readonly useAttrs: UnwrapRef<typeof import('vue')['useAttrs']>
     readonly useCssModule: UnwrapRef<typeof import('vue')['useCssModule']>
     readonly useCssVars: UnwrapRef<typeof import('vue')['useCssVars']>
+    readonly useErrorStore: UnwrapRef<typeof import('./src/stores/error')['useErrorStore']>
     readonly useId: UnwrapRef<typeof import('vue')['useId']>
     readonly useModel: UnwrapRef<typeof import('vue')['useModel']>
     readonly usePageStore: UnwrapRef<typeof import('./src/stores/store')['usePageStore']>
--- src/stores/error.ts
+++ src/stores/error.ts
@@ -4,6 +4,9 @@ export const useErrorStore = defineStore('error-store', () => {
   const activeError = ref<null | CustomError>(null)
 
   const setError = ({ error, customCode }: { error: string; customCode: number }) => {
+    // const err = new Error(error) as CustomError
+    // err.customCode = customCode
+    // activeError.value = err
     activeError.value = Error(error)
     activeError.value.customCode = customCode
   }
