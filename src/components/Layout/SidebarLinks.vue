b9db98ec446d573692251e86ea7bd8e926e74b63
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-06-13 19:20:43 +0300

    creating if/else for links without to to make custom div

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -13,27 +13,27 @@ const filtredLinks = props.links.filter((link): link is LinkProp & { to: string 
 </script>
 
 <template>
-  <RouterLink
-    exact-active-class="text-primary bg-muted"
-    v-for="link in filtredLinks"
-    :key="link.title"
-    :to="link.to"
-    class="link"
-  >
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
-  </RouterLink>
-
-  <div class="nav-link cursor-pointer">
-    <!-- <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span> -->
-  </div>
+  <template v-for="link in links" :key="link.title">
+    <RouterLink
+      v-if="link.to"
+      exact-active-class="text-primary bg-muted"
+      :to="link.to"
+      class="nav-link"
+    >
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </RouterLink>
+    <div v-else class="nav-link cursor-pointer">
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </div>
+  </template>
 </template>
 
 <style scoped>
 @reference "@/assets/globals.css";
 
-:deep(.nav-link) {
+.nav-link {
   @apply flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground;
 }
 </style>
