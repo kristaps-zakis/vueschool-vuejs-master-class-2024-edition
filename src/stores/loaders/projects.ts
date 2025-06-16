c41595909864798bab296063ba58d35509dbf29b
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-06-16 09:24:35 +0300

    adding simple memoaize to app

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,13 +1,14 @@
 import { projectsQuery } from '@/utils/supaQueries'
 import type { Projects } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 
 export const useProjectStore = defineStore('projcets-store', () => {
   const projects = ref<Projects | null>(null)
 
-  const getProjects = async () => {
-    if (projects.value?.length) return
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
-    const { data, error, status } = await projectsQuery
+  const getProjects = async () => {
+    const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
     projects.value = data
