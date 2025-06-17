ba30ae7970761a5844f27ccc777a58e2d19dad34
Kristaps Zaķis <kristaps.zakis@gmail.com>
2025-06-17 10:39:18 +0300

    creating emmit functionality

--- src/components/AppInPlaceEdit/AppInPlaceEditText.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditText.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const inputValue = defineModel()
+defineEmits(['commit'])
 </script>
 
 <template>
@@ -8,5 +9,7 @@ const inputValue = defineModel()
     placeholder="Default value"
     class="w-full p-1 bg-transparent focus:outline-none focus:border-none focus:bg-gray-800 focus:rounded-md"
     v-model="inputValue"
+    @blur="$emit('commit')"
+    @keyup.enter="($event.target as HTMLInputElement).blur()"
   />
 </template>
--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -33,7 +33,9 @@ await getProject(slug)
   <Table v-if="project">
     <TableRow>
       <TableHead> Name </TableHead>
-      <TableCell><AppInPlaceEditText v-model="project.name" /> </TableCell>
+      <TableCell
+        ><AppInPlaceEditText v-model="project.name" @commit="console.log('changed')" />
+      </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description </TableHead>
